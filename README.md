dffe7c759e690e14855d23feab0d8b2ce18eb544
Siddhesh Mangela <sidthecool007@gmail.com>
2021-03-25 11:04:54 +0530

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Folio vue
+
+Portfolio source code
+
+1. copy src/db/db.json.sample as src/db/db.json
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>Hello</template>
+
+<script setup></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
