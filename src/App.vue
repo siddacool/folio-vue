21da94d2f60f1438353d4d858fe4a5a4daffd113
Siddhesh Mangela <sidthecool007@gmail.com>
2021-03-30 11:40:17 +0530

    server init

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,9 @@
 <template>
-  <button @click="handleGetHomeSlides">Yo</button>
-
-  <button @click="handleGetHomeSlideRemove">Delete</button>
-
-  {{ homeSlides }}
+  <Hello />
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import homeSlidesApi from './api/home-slides';
-
-const homeSlides = ref([]);
-
-const handleGetHomeSlides = async () => {
-  try {
-    const homeSlidesFetched = await homeSlidesApi.getHomeSlides();
-
-    homeSlides.value = homeSlidesFetched;
-  } catch (e) {
-    console.dir(e);
-  }
-};
-
-const handleGetHomeSlideRemove = async () => {
-  try {
-    await homeSlidesApi.removeHomeSlideById(1);
-    const homeSlidesFetched = await homeSlidesApi.getHomeSlides();
-
-    homeSlides.value = homeSlidesFetched;
-  } catch (e) {
-    console.dir(e);
-  }
-};
+import Hello from 'components/Hello.vue';
 </script>
 
 <style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/folio/',
   assetsDir: 'assets',
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      components: path.resolve(__dirname, '/src/components'),
+      // eslint-disable-next-line no-undef
+    },
+  },
 });
